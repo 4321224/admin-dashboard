1021567d04cbbff88bbc81032c57e602dfcfbe17
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-16 08:22:34 +0700

    process app

--- server/app.js
+++ server/app.js
@@ -1,10 +1,32 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from 'cors'
-import dotenv from 'dotenv'
+import cors from "cors";
+import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import mongoose from "mongoose";
 
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-// how to connect mongoose in nodejs?
+
+mongoose.set("strictQuery", false)
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Running Server on Port: ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(`${error} did not connect`));
